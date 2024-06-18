056fad7c65e84c1f1f5ac5fc9edaeb486445f5c5
Nico Juric <nicojuric@gmail.com>
2024-06-18 12:32:14 +0200

    led display

--- LED_Display.py
+++ LED_Display.py
@@ -1,81 +1,88 @@
+standard_input = "778"
 led_display_tuple = (
-    """
-        ###
-        # #
-        # #
-        # #
-        ###
-    """,
-    """
-          #
-          #
-          #
-          #
-          #
-    """,
-    """
-        ###
-          #
-        ###
-        #
-        ###
-    """,
-    """
-        ###
-          #
-        ###
-          #
-        ###
-    """,
-    """
-        # #
-        # #
-        ###
-          #
-          #
-    """,
-    """
-        ###
-        #
-        ###
-          #
-        ###
-    """,
-    """
-        ###
-        #
-        ###
-        # #
-        ###
-    """,
-    """
-        ###
-          #
-          #
-          #
-          #
-    """,
-    """
-        ###
-        # #
-        ###
-        # #
-        ###
-    """,
-    """
-        ###
-        # #
-        ###
-          #
-        ###
-    """,
+    [
+        "###",
+        "# #",
+        "# #",
+        "# #",
+        "###"
+    ],
+    [
+        "  #",
+        "  #",
+        "  #",
+        "  #",
+        "  #"
+    ],
+    [
+        "###",
+        "  #",
+        "###",
+        "#  ",
+        "###"
+    ],
+    [
+        "###",
+        "  #",
+        "###",
+        "  #",
+        "###"
+    ],
+    [
+        "# #",
+        "# #",
+        "###",
+        "  #",
+        "  #"
+    ],
+    [
+        "###",
+        "#  ",
+        "###",
+        "  #",
+        "###"
+    ],
+    [
+        "###",
+        "#  ",
+        "###",
+        "# #",
+        "###"
+    ],
+    [
+        "###",
+        "  #",
+        "  #",
+        "  #",
+        "  #"
+    ],
+    [
+        "###",
+        "# #",
+        "###",
+        "# #",
+        "###"
+    ],
+    [
+        "###",
+        "# #",
+        "###",
+        "  #",
+        "###"
+    ]
 )
+
 user_input = input("Input the integers to display\n")
 n_to_display = []
 
-for i in user_input:
-    index = int(i)
+# Collect the lines for each digit
+for digit in user_input:
+    index = int(digit)
     n_to_display.append(led_display_tuple[index])
-    
-for display in n_to_display:
-    print(display)
+
+# Print the digits side by side
+for i in range(5):  # Each digit has 5 lines
+    line = ""
+    for digit in n_to_display:
+        line += digit[i] + " "  # Adjust spaces between digits
+    print(line)
